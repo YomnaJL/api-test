e0e4bd5c67d3dfa3e16fcc260794a44c020321a7
YomnaJL <yomnajlassikhardani@gmail.com>
2026-05-31 19:48:51 +0100

    fix

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
-import { add, multiply } from "./helpers.js";
-import { fetchUser } from "./api.js";
+const { add, multiply } = require("./helpers");
+const { fetchUser } = require("./api");
 
 async function main() {
-  console.log("add(3, 5) =", add(3, 5));
-  console.log("multiply(4, 6) =", multiply(4, 6));
+  const addResult      = add(3, 5);
+  const multiplyResult = multiply(4, 6);
+  const user           = await fetchUser(1);
 
-  const user = await fetchUser(1);
-  console.log("Utilisateur récupéré :", user);
+  console.log("add(3, 5) =", addResult);
+  console.log("multiply(4, 6) =", multiplyResult);
+
+  return { add: addResult, multiply: multiplyResult, user };
 }
 
-main();
+module.exports = { main };
